929839d1fe8c4778b06a1fbbff1762783f5b4527
David Goodman <GiantSox@users.noreply.github.com>
2022-09-25 15:34:33 -0700

    Trying out VS2019's CMake integration for the first time. Hopefully I don't regret this!

---
+++ tiltfive-steamvr/tiltfive-steamvr/tiltfive-steamvr.cpp
@@ -0,0 +1,12 @@
+﻿// tiltfive-steamvr.cpp : Defines the entry point for the application.
+//
+
+#include "tiltfive-steamvr.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
